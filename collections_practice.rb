36ecbf8d00c4abec82c10601bae6fc6e8ae0b90e
Jesse McReady <jessemcready@gmail.com>
2018-06-28 20:42:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
 # your code goes here
+def begins_with_r(*args)
+  
+end
+
+def contain_a(*args)
+  
+end
+
+def first_wa(*args)
+  
+end
+
+def remove_non_strings(*args)
+  
+end
+
+def count_elements(*args)
+  
+end
+
+def merge_data(keys, data)
+  
+end
+
+def find_cool
+  
+end
+
+def organize_schools(*schools)
+  
+end
