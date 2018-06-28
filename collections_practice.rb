a7fc3d2061e154d97a618aebacf412edfe95c79b
Jesse McReady <jessemcready@gmail.com>
2018-06-28 21:40:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,16 +58,27 @@ end
 def merge_data(keys, data)
   merged = []
   
+  # first loop through our keys to get, in our case, the first names of our people
   keys.each do |pair|
+    # then for each first_name=>"<name>" pair (Blake/Ashley)
     pair.each do |key, value|
+      # we create a new empty hash person
       merged_person = {}
+      # and begin with first giving that hash person a key value pair of first_name: "Blake/Ashley"
       merged_person[key] = value
+      # then we have to traverse our data argument
       data.each do |person|
+        # at each person that is already in our data array, get the hash for each person
         person.each do |name, characteristics|
+          # name will either be Blake or Ashley, and characteristics is the hash of awesomeness, height, and last_name
+          # if we get to the person we originally started back with merged_person[key] = value
           if name == merged_person[:first_name]
+            # we want to traverse our characteristics hash 
             characteristics.each do |char, rating|
+              # and add those characteristics to our merged_person hash, so that they are all on the same level
               merged_person[char] = rating
             end
+            # push our merged person into our merged array and return it at the end
             merged << merged_person
           end
         end
