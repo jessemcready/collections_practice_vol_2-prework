99b24a7289592835dfd29d5e30bcc0295eb634ee
Jesse McReady <jessemcready@gmail.com>
2018-06-28 21:32:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,9 +62,12 @@ def merge_data(keys, data)
   keys.each do |pair|
     pair.each do |key, value|
       merged_person[key] = value
+      puts "MERGED_PERSON #{merged_person}"
       data.each do |person|
         person.each do |name, characteristics|
-          if name == merged_person[:name]
+          if name == merged_person[:first_name]
+            
+          end
         end
       end
     end
